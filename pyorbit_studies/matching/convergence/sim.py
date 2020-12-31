5609be9196f890c3a521332660cd74316b2e2b3f
austin-hoover <ahoover7@tennessee.edu>
2020-12-30 19:28:31 -0500

    Add study: matching algorithm performance

---
+++ pyorbit_studies/matching/convergence/sim.py
@@ -0,0 +1,120 @@
+"""
+This script 
+"""
+
+# Standard 
+import sys
+import copy
+# Third party
+import numpy as np
+from tqdm import tqdm, trange
+# PyORBIT
+from bunch import Bunch
+from spacecharge import SpaceChargeCalc2p5D
+from orbit.analysis import (
+    AnalysisNode,
+    add_analysis_nodes, 
+    get_analysis_nodes_data, 
+    clear_analysis_nodes_data)
+from orbit.analysis.analysis import covmat2vec
+from orbit.coupling import bogacz_lebedev as BL
+from orbit.envelope import Envelope
+from orbit.lattice import AccLattice, AccNode, AccActionsContainer
+from orbit.space_charge.envelope import set_env_solver_nodes, set_perveance
+from orbit.space_charge.sc2p5d.scLatticeModifications import setSC2p5DAccNodes
+from orbit.teapot import TEAPOT_Lattice
+from orbit.utils import helper_funcs as hf
+# Local
+sys.path.append('/Users/46h/Research/code/accphys') 
+from tools.utils import delete_files_not_folders
+    
+    
+# Settings
+#------------------------------------------------------------------------------
+
+# General
+mass = 0.93827231 # GeV/c^2
+energy = 1.0 # GeV/c^2
+
+# Lattice
+latfile = '_latfiles/fodo_skew_quadstart.lat'
+latseq = 'fodo'
+fringe = False
+
+# Initial beam
+mode = 1
+eps = 50e-6 # intrinsic emitance
+ex_frac = 0.5 # ex/eps
+nu = np.radians(90) # x-y phase difference
+
+# Space charge solver
+max_solver_spacing = 0.05 # [m]
+min_solver_spacing = 1e-6
+
+# Matching
+match = True 
+tol = 1e-4 # absolute tolerance for cost function
+verbose = 2 # {0 (silent), 1 (report once at end), 2 (report at each step)}
+perturb_radius = 0. # if nonzero, perturb the matched envelope
+method = 'replace_by_avg'
+
+delete_files_not_folders('_output/')
+
+
+#------------------------------------------------------------------------------
+
+intensity = 4e14
+nturns = 15
+
+# Match and store optimizer history
+print 'Matching. method =', method
+lattice = hf.lattice_from_file(latfile, latseq, fringe)
+env = Envelope(eps, mode, ex_frac, mass, energy, lattice.getLength())
+env.set_spacecharge(intensity)
+solver_nodes = set_env_solver_nodes(lattice, env.perveance, max_solver_spacing)
+result = env.match(lattice, solver_nodes, method=method, verbose=0)
+
+# Using the seed from each iteration, track and store the turn-by-turn 
+# envelope parameters at the lattice entrance
+print 'Collecting s-dependent data.'
+tbt_params_list = []
+for twiss_params in tqdm(result.history):
+    env.fit_twiss4D(twiss_params)
+    tbt_params = env.track_store_params(lattice, nturns)
+    tbt_params_list.append(tbt_params)
+np.save('_output/data/tbt_params_list.npy', tbt_params_list)
+
+# Using the seed from each iteration, track and store the s-dependent
+# envelope parameters
+print 'Collecting turn-by-turn data.'
+monitor_nodes = add_analysis_nodes(lattice, kind='env_monitor')
+sdep_params_list = []
+for twiss_params in tqdm(result.history):
+    env.fit_twiss4D(twiss_params)
+    env.track(lattice)
+    sdep_params = get_analysis_nodes_data(monitor_nodes, 'env_params')
+    sdep_params_list.append(sdep_params)
+    clear_analysis_nodes_data(monitor_nodes)
+np.save('_output/data/sdep_params_list.npy', sdep_params_list)
+np.save('_output/data/positions.npy', 
+        get_analysis_nodes_data(monitor_nodes, 'position'))
+
+
+
+
+# def cost_func(env, lattice):
+#     residuals = env._mismatch_error(lattice)
+#     return 0.5 * np.sum(residuals**2)
+#
+# costs_arr = []
+# for method in ('lsq', 'replace_by_avg'):
+#     print 'Method =', method
+#     costs = []
+#     for intensity in tqdm(intensities):
+#         lattice = hf.lattice_from_file(latfile, latseq, fringe)
+#         env.set_spacecharge(intensity)
+#         solver_nodes = set_env_solver_nodes(lattice, env.perveance, max_solver_spacing)
+#         env.match(lattice, solver_nodes, method=method, tol=tol, verbose=0)
+#         costs.append(cost_func(env, lattice))
+#     costs_arr.append(costs)
+# np.save('_output/data/costs_arr.npy', costs_arr)
