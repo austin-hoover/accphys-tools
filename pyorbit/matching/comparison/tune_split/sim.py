9d2c41460fe2a9ff93501be6426f6a9f2114afa4
austin-hoover <ahoover7@tennessee.edu>
2021-05-19 10:17:01 -0400

    Update scipts in /tune_split/

--- pyorbit/matching/comparison/tune_split/sim.py
+++ pyorbit/matching/comparison/tune_split/sim.py
@@ -1,34 +1,36 @@
 """
-This script examines the matched solutions in a FODO lattice as the difference 
-between the horizontal and vertical tunes is varied.
+This script examines the matched solutions in a FODO lattice as the 
+horizontal and vertical tunes are split.
 """
-
-# Standard 
 import sys
-# Third party
 import numpy as np
-from tqdm import tqdm
-# PyORBIT
+from tqdm import tqdm, trange
+
 from bunch import Bunch
-from orbit.analysis import add_analysis_nodes, get_analysis_nodes_data
-from orbit.coupling import bogacz_lebedev as BL
-from orbit.envelope import Envelope
+from spacecharge import SpaceChargeCalc2p5D
+from orbit.analysis import AnalysisNode
+from orbit.analysis import add_analysis_nodes
+from orbit.analysis import get_analysis_nodes_data
+from orbit.envelope import DanilovEnvelope
 from orbit.lattice import AccLattice, AccNode, AccActionsContainer
 from orbit.space_charge.envelope import set_env_solver_nodes, set_perveance
+from orbit.space_charge.sc2p5d.scLatticeModifications import setSC2p5DAccNodes
 from orbit.teapot import TEAPOT_Lattice
+from orbit.twiss import twiss
 from orbit.utils import helper_funcs as hf
-# Local
-sys.path.append('/Users/46h/Research/code/accphys')
+
+sys.path.append('/Users/46h/Research/code/accphys') 
 from tools.utils import delete_files_not_folders
 
     
 # Settings
 #------------------------------------------------------------------------------
-
 # General
 mass = 0.93827231 # GeV/c^2
-energy = 1.0 # GeV
-intensity = 1e14
+kin_energy = 1.0 # GeV
+intensity = 3e14
+bunch_length = 5.0 # m
+match = True
 
 # Lattice
 mux = 80 # total x phase advance [deg]
@@ -40,23 +42,21 @@ start = 'quad' # lattice entrance (the center of quad vs. center of drift)
 fringe = False # whether to include fringe fields
 
 # Initial beam
-mode = 1
-eps = 50e-6 # intrinsic emitance
-ex_frac = 0.5 # ex/eps
+mode = 2
+eps = 40e-6 # intrinsic emitance
+eps_x_frac = 0.5 # ex/eps
 nu = np.radians(90) # x-y phase difference
 
 # Matching
-tol = 1e-4 # absolute tolerance for cost function
-verbose = 2 # {0 (silent), 1 (report once at end), 2 (report at each step)}
-
-# Space charge solver
+match = True
+verbose = 2 
 max_solver_spacing = 0.01 # [m]
 
 # Output data locations
 files = {
     'positions': '_output/data/positions.npy', 
     'phase_adv_diffs': '_output/data/phase_adv_diffs.npy', 
-    'tracked_env_params_list': '_output/data/tracked_env_params_list.npy',
+    'tracked_params_list': '_output/data/tracked_params_list.npy',
     'transfer_mats': '_output/data/transfer_mats.npy',
 }
 data = {key:None for key in files.keys()}
@@ -66,29 +66,24 @@ delete_files_not_folders('_output')
 
 # Simulation
 #------------------------------------------------------------------------------
-
-env = Envelope(eps, mode, ex_frac, mass, energy, length)
-tracked_env_params_list, transfer_mats = [], []
-
+env = DanilovEnvelope(eps, mode, eps_x_frac, mass, kin_energy, bunch_length)
+tracked_params_list, transfer_mats = [], []
 for muy in tqdm(muy_list):
-    
     lattice = hf.fodo_lattice(mux, muy, length, fill_fac, angle, start)
     env.match_bare(lattice)
-    env.set_spacecharge(intensity)
+    env.set_intensity(intensity)
     solver_nodes = set_env_solver_nodes(lattice, env.perveance, max_solver_spacing)
-    if intensity > 0:
+    if match and intensity > 0:
         env.match(lattice, solver_nodes, verbose=0)
-    
     transfer_mats.append(env.transfer_matrix(lattice))
     env_monitor_nodes = add_analysis_nodes(lattice, kind='env_monitor')
     env.track(lattice)
-    tracked_env_params = get_analysis_nodes_data(env_monitor_nodes, 'env_params')
-    tracked_env_params_list.append(tracked_env_params)
+    tracked_params = get_analysis_nodes_data(env_monitor_nodes, 'env_params')
+    tracked_params_list.append(tracked_params)
 
-# Save data
 data['positions'] = get_analysis_nodes_data(env_monitor_nodes, 'position')
 data['phase_adv_diffs'] = [muy - mux for muy in muy_list]
-data['tracked_env_params_list'] = tracked_env_params_list
+data['tracked_params_list'] = tracked_params_list
 data['transfer_mats'] = transfer_mats
 
 for key in files.keys():
