333d431853e4a6575070049b08809e1b379d7b2b
austin-hoover <ahoover7@tennessee.edu>
2021-05-19 10:35:13 -0400

    Update scripts in /optimizer_progress/

--- pyorbit/matching/convergence/optimizer_progress/sim.py
+++ pyorbit/matching/convergence/optimizer_progress/sim.py
@@ -8,41 +8,34 @@ beam approaches the matched solution.
 Unfortunately there is no `callback` option for scipy.optimize.least_squares,
 so we cannot view its progress.
 """
-
-# Standard 
 import sys
-import copy
-# Third party
 import numpy as np
 from tqdm import tqdm, trange
-# PyORBIT
+
 from bunch import Bunch
 from spacecharge import SpaceChargeCalc2p5D
-from orbit.analysis import (
-    AnalysisNode,
-    add_analysis_nodes, 
-    get_analysis_nodes_data, 
-    clear_analysis_nodes_data)
-from orbit.analysis.analysis import covmat2vec
-from orbit.coupling import bogacz_lebedev as BL
-from orbit.envelope import Envelope
+from orbit.analysis import AnalysisNode
+from orbit.analysis import add_analysis_nodes
+from orbit.analysis import get_analysis_nodes_data
+from orbit.envelope import DanilovEnvelope
 from orbit.lattice import AccLattice, AccNode, AccActionsContainer
 from orbit.space_charge.envelope import set_env_solver_nodes, set_perveance
 from orbit.space_charge.sc2p5d.scLatticeModifications import setSC2p5DAccNodes
 from orbit.teapot import TEAPOT_Lattice
+from orbit.twiss import twiss
 from orbit.utils import helper_funcs as hf
-# Local
+
 sys.path.append('/Users/46h/Research/code/accphys') 
 from tools.utils import delete_files_not_folders
     
     
 # Settings
 #------------------------------------------------------------------------------
-
 # General
 intensity = 2e14
 mass = 0.93827231 # GeV/c^2
-energy = 1.0 # GeV/c^2
+kin_energy = 1.0 # GeV/c^2
+bunch_length = 5.0
 
 # Lattice
 latfile = '_latfiles/fodo_skew_driftstart.lat'
@@ -52,7 +45,7 @@ fringe = False
 # Initial beam
 mode = 1
 eps = 50e-6 # intrinsic emitance
-ex_frac = 0.3 # ex/eps
+eps_x_frac = 0.25 # ex/eps
 nu = np.radians(90) # x-y phase difference
 
 # Space charge solver
@@ -64,7 +57,7 @@ match = True
 tol = 1e-4 # absolute tolerance for cost function
 verbose = 2 # {0 (silent), 1 (report once at end), 2 (report at each step)}
 perturb_radius = 0. # if nonzero, perturb the matched envelope
-method = 'replace_by_avg'
+method = 'replace_avg'
 
 delete_files_not_folders('_output/')
 
@@ -76,8 +69,8 @@ nturns = 15
 # Match and store optimizer history
 print 'Matching.'
 lattice = hf.lattice_from_file(latfile, latseq, fringe)
-env = Envelope(eps, mode, ex_frac, mass, energy, lattice.getLength())
-env.set_spacecharge(intensity)
+env = DanilovEnvelope(eps, mode, eps_x_frac, mass, kin_energy, length=bunch_length)
+env.set_intensity(intensity)
 solver_nodes = set_env_solver_nodes(lattice, env.perveance, max_solver_spacing)
 result = env.match(lattice, solver_nodes, method=method, verbose=2)
 
@@ -87,24 +80,25 @@ print 'Collecting turn-by-turn data.'
 tbt_params_list = []
 costs = []
 for twiss_params in tqdm(result.history):
-    env.fit_twiss4D(twiss_params)
-    costs.append(env._mismatch_error(lattice, ssq=True))
+    env.set_twiss4D(twiss_params)
+    costs.append(0.5 * np.sum(env._residuals(lattice)))
     tbt_params = env.track_store_params(lattice, nturns)
     tbt_params_list.append(tbt_params)
 np.save('_output/data/tbt_params_list.npy', tbt_params_list)
 np.save('_output/data/costs.npy', costs)
 
 # Using the seed from each iteration, track and store the s-dependent
-# envelope parameters
+# envelope parameters.
 print 'Collecting s-dependent data.'
 monitor_nodes = add_analysis_nodes(lattice, kind='env_monitor')
 sdep_params_list = []
 for twiss_params in tqdm(result.history):
-    env.fit_twiss4D(twiss_params)
+    env.set_twiss4D(twiss_params)
     env.track(lattice)
     sdep_params = get_analysis_nodes_data(monitor_nodes, 'env_params')
     sdep_params_list.append(sdep_params)
-    clear_analysis_nodes_data(monitor_nodes)
+    for monitor_node in monitor_nodes:
+        monitor_node.clear_data()
 np.save('_output/data/sdep_params_list.npy', sdep_params_list)
 np.save('_output/data/positions.npy', 
         get_analysis_nodes_data(monitor_nodes, 'position'))
