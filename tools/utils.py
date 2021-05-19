4a85948a51fae92986b05690a7de0ec02db6a80a
austin-hoover <ahoover7@tennessee.edu>
2021-05-19 16:32:41 -0400

    Add function to save/load stacked ragged arrays

--- tools/utils.py
+++ tools/utils.py
@@ -127,6 +127,35 @@ def vec2mat(moment_vec):
         Sigma[i, j] = moment
     return symmetrize(Sigma)
     
+    
+# The following three functions are from Tony Yu's blog post: https://tonysyu.github.io/ragged-arrays.html#.YKVwQy9h3OR
+    
+def stack_ragged(array_list, axis=0):
+    """Stacks list of arrays along first axis.
+    
+    Example: (25, 4) + (75, 4) -> (100, 4). It also returns the indices at
+    which to split the stacked array to regain the original list of arrays.
+    """
+    lengths = [np.shape(array)[axis] for array in array_list]
+    idx = np.cumsum(lengths[:-1])
+    stacked = np.concatenate(array_list, axis=axis)
+    return stacked, idx
+    
+
+def save_stacked_array(filename, array_list, axis=0):
+    """Save list of ragged arrays as single stacked array. The index from
+    `stack_ragged` is also saved."""
+    stacked, idx = stack_ragged(array_list, axis=axis)
+    np.savez(filename, stacked_array=stacked, stacked_index=idx)
+    
+    
+def load_stacked_arrays(filename, axis=0):
+    """"Load stacked ragged array from .npz file as list of arrays."""
+    npz_file = np.load(filename)
+    idx = npz_file['stacked_index']
+    stacked = npz_file['stacked_array']
+    return np.split(stacked, idx, axis=axis)
+    
 
 # Math
 def cov2corr(cov_mat):
