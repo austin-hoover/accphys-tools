c689e21213f893b91971334fff193c62a72a6c14
austin-hoover <ahoover7@tennessee.edu>
2021-08-07 16:38:32 -0400

    Rename folder and bring one script up to date

--- danilov_distribution/matching/tracking/turn_by_turn/onebeam/sim.py
+++ danilov_distribution/matching/tracking/turn_by_turn/onebeam/sim.py
@@ -6,25 +6,32 @@ turn-by-turn envelope parameters, bunch moments, and/or bunch coordinates.
 The saved file formats are '.npy', which is convenient for storing multi-dim
 arrays. They can be loaded by calling `np.load(filename)`.
 """
+from __future__ import print_function
 import sys
+import os
+
 import numpy as np
-from tqdm import tqdm, trange
+from tqdm import tqdm
+from tqdm import trange
 
 from bunch import Bunch
 from spacecharge import SpaceChargeCalc2p5D
-from orbit.analysis import AnalysisNode
-from orbit.analysis import add_analysis_nodes
-from orbit.analysis import get_analysis_nodes_data
+from orbit.diagnostics import BunchMonitorNode
+from orbit.diagnostics import BunchStatsNode
+from orbit.diagnostics import DanilovEnvelopeBunchMonitorNode
+from orbit.diagnostics import add_analysis_node
+from orbit.diagnostics import add_analysis_nodes
 from orbit.envelope import DanilovEnvelope
-from orbit.lattice import AccLattice, AccNode, AccActionsContainer
-from orbit.space_charge.envelope import set_env_solver_nodes, set_perveance
+from orbit.lattice import AccLattice
+from orbit.lattice import AccNode
+from orbit.lattice import AccActionsContainer
+from orbit.space_charge.envelope import set_env_solver_nodes
+from orbit.space_charge.envelope import set_perveance
 from orbit.space_charge.sc2p5d.scLatticeModifications import setSC2p5DAccNodes
 from orbit.teapot import TEAPOT_Lattice
 from orbit.twiss import twiss
 from orbit.utils import helper_funcs as hf
-
-sys.path.append('/Users/46h/Research/code/accphys') 
-from tools.utils import delete_files_not_folders
+from orbit.utils.general import delete_files_not_folders
 
     
 # Settings
@@ -32,23 +39,23 @@ from tools.utils import delete_files_not_folders
 # General
 mass = 0.93827231 # [GeV/c^2]
 kin_energy = 1.0 # [GeV/c^2]
-intensity = 0.0e14
-bunch_length = 150.0 # [m]
-nparts = int(1e5)
+intensity = 1.5e14
+n_parts = int(1e5)
 ntestparts = 100
-nturns_track = 100
-track_bunch = False
-store_bunch_coords = False
+n_turns_track = 10
+track_bunch = True
+store_bunch_coords = True
 
 # Lattice
-latfile = '_latfiles/SNSring_from_openxal.lat'
-latseq = 'ring_ring'
+madx_file = '_input/fodo_quadstart.lat'
+madx_seq = 'fodo'
 fringe = False
 
 # Initial beam
+bunch_length = 150.0 # [m]
 mode = 2
-eps = 40e-6 # intrinsic emitance
-eps_x_frac = 0.5 # ex/eps
+eps_l = 40e-6 # nonzero intrinsic emittance [m rad]
+eps_x_frac = 0.5 # eps_x / eps_l
 nu = np.radians(90) # x-y phase difference
 
 # Space charge solver
@@ -61,7 +68,7 @@ match = True
 tol = 1e-4 # absolute tolerance for cost function
 verbose = 2 # {0 (silent), 1 (report once at end), 2 (report at each step)}
 perturb_radius = 0.0 # between 0 (no effect) and 1.0
-method = 'auto' # 'lsq' or 'replace_by_avg'
+method = 'auto' # {'lsq', 'replace_by_avg'}
 
 # Output data locations
 files = {
@@ -72,43 +79,50 @@ files = {
     'transfer_matrix': '_output/data/transfer_matrix.npy'
 }
 
-delete_files_not_folders('_output/')
+delete_files_not_folders('./_output/')
     
         
 # Envelope
 #------------------------------------------------------------------------------
 
 # Create envelope matched to bare lattice
-lattice = hf.lattice_from_file(latfile, latseq, fringe)
-env = DanilovEnvelope(eps, mode, eps_x_frac, mass, kin_energy, length=bunch_length)
+lattice = TEAPOT_Lattice()
+lattice.readMADX(madx_file, madx_seq)
+lattice.set_fringe(fringe)
+env = DanilovEnvelope(eps_l, mode, eps_x_frac, mass, kin_energy, length=bunch_length)
 env.match_bare(lattice, '2D') # if '4D', and unequal tunes, beam will have 0 area
     
 # Create envelope 
 env.set_intensity(intensity)
 solver_nodes = set_env_solver_nodes(lattice, env.perveance, max_solver_spacing)
 if match and intensity > 0:
-    print 'Matching.'
+    print('Matching.')
     env.match(lattice, solver_nodes, tol=tol, verbose=verbose, method=method)
 if perturb_radius > 0:
-    print 'Perturbing envelope (radius = {:.2f}).'.format(perturb_radius)
+    print('Perturbing envelope (radius = {:.2f}).'.format(perturb_radius))
     env.perturb(perturb_radius)
 init_params = np.copy(env.params)
     
 # Get linear transfer matrix
 M = env.transfer_matrix(lattice)
 mux, muy = 360 * env.tunes(lattice)
-print 'Transfer matrix is {}stable.'.format('' if twiss.is_stable(M) else 'un')
-print '    mux, muy = {:.3f}, {:.3f} deg'.format(mux, muy)
+print('Transfer matrix is {}stable.'.format('' if twiss.is_stable(M) else 'un'))
+print('    mux, muy = {:.3f}, {:.3f} deg'.format(mux, muy))
+
+# Add envelope monitor node.
+first_node = lattice.getNodes()[0]
+env_monitor_node = add_analysis_node(DanilovEnvelopeBunchMonitorNode, lattice, first_node)
 
 # Track envelope
-print 'Tracking envelope.'
-env_monitor_node = AnalysisNode(0.0, 'env_monitor')
-hf.add_node_at_start(lattice, env_monitor_node)
-env.track(lattice, nturns_track, ntestparts, progbar=True)
+print('Tracking envelope.')
+env.track(lattice, n_turns_track, ntestparts, progbar=True)
 
 # Save data
-for key in ('env_params', 'testbunch_coords'):
-    np.save(files[key], env_monitor_node.get_data(key, 'all_turns'))
+env_data_tbt = env_monitor_node.get_data()
+env_params_tbt = [env_data.env_params for env_data in env_data_tbt]
+testbunch_coords = [env_data.coords for env_data in env_data_tbt]
+np.save(files['env_params'], env_params_tbt)
+np.save(files['testbunch_coords'], testbunch_coords)
 np.save(files['transfer_matrix'], M)
 np.savetxt('_output/data/mode.txt', [mode])
 
@@ -117,31 +131,32 @@ np.savetxt('_output/data/mode.txt', [mode])
 #------------------------------------------------------------------------------
 if track_bunch:
     
-    # Create lattice with space charge nodes
-    lattice = hf.lattice_from_file(latfile, latseq, fringe)
+    # Create lattice with space charge nodes.
+    lattice = TEAPOT_Lattice()
+    lattice.readMADX(madx_file, madx_seq)
+    lattice.set_fringe(fringe)
     lattice.split(max_solver_spacing)    
     if intensity > 0:
         calc2p5d = SpaceChargeCalc2p5D(*gridpts)
         sc_nodes = setSC2p5DAccNodes(lattice, min_solver_spacing, calc2p5d)
 
-    # Create bunch
+    # Create bunch.
     env.params = init_params
-    bunch, params_dict = env.to_bunch(nparts, no_env=True)
+    bunch, params_dict = env.to_bunch(n_parts, no_env=True)
 
-    # Add analysis nodes
-    bunch_monitor_node = AnalysisNode(0.0, 'bunch_monitor')
-    bunch_stats_node = AnalysisNode(0.0, 'bunch_stats')
-    hf.add_node_at_start(lattice, bunch_stats_node)
+    # Add analysis nodes.
+    first_node = lattice.getNodes()[0]
     if store_bunch_coords:
-        hf.add_node_at_start(lattice, bunch_monitor_node)
+        bunch_monitor_node = add_analysis_node(BunchMonitorNode, lattice, first_node)   
+    bunch_stats_node = add_analysis_node(BunchStatsNode, lattice, first_node)   
     
-    # Track bunch
-    print 'Tracking bunch.'
-    hf.track_bunch(bunch, params_dict, lattice, nturns_track)
+    # Track bunch.
+    print('Tracking bunch.')
+    hf.track_bunch(bunch, params_dict, lattice, n_turns_track)
     
-    # Save data
-    moments = bunch_stats_node.get_data('bunch_moments', 'all_turns')
-    np.save(files['bunch_moments'], moments)
+    # Save data.
+    bunch_moments = bunch_stats_node.get_data()
+    np.save(files['bunch_moments'], bunch_moments)
     if store_bunch_coords:
-        coords = bunch_monitor_node.get_data('bunch_coords', 'all_turns')
-        np.save(files['bunch_coords'], coords)
+        bunch_coords = bunch_monitor_node.get_data()
+        np.save(files['bunch_coords'], bunch_coords)
