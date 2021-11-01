70bff6c83b3691e8162514f2cc5ecf3a5a5c66ab
austin-hoover <ahoover7@tennessee.edu>
2021-11-01 10:52:52 -0400

    Add 4D ART implementation

--- emittance_measurement_4D/tomography/reconstruct.py
+++ emittance_measurement_4D/tomography/reconstruct.py
@@ -148,3 +148,96 @@ def transform(Z, V, grid, new_grid=None):
     Z[np.isnan(Z)] = 0.0
     Z = Z.reshape([len(xi) for xi in new_grid])
     return Z, new_grid
+
+
+
+def art4D(projections, tmats, rec_grid_centers, screen_edges_x, screen_edges_y):
+    """Direct four-dimensional algebraic reconstruction (ART).
+    
+    We set up the linear system rho = P psi. Assume the x-x'-y-y' grid at the reconstruction
+    grid has Nr**4 bins, the x-y grid on the screen has Ns**2 bins, and that there are n
+    measurements. Then rho is a vector with n*Ns**2 elements of the measured density on the
+    screen and psi is a vector with Nr**4 elements. P[i, j] = 1.0 if the jth bin center in 
+    the reconstruction grid ends up in the ith bin on the screen, or 0.0 otherwise. 
+    
+    P is a very sparse matrix. Currently, scipy.sparse.linalg.lsqr is used. A grid size of
+    N = 50 has used successfuly, but N = 75 lead to an 'out of memory' error.
+    
+    Parameters
+    ----------
+    projections : list[ndarray, shape (Nsx, Nsy)]
+        List of measured projections on the x-y plane.
+    tmats : list[ndarray, shape (4, 4)]
+        List of transfer matrices from the reconstruction location to the measurement location.
+    rec_grid_centers : list[ndarray, shape (Nr,)]
+        Grid center coordinates in [x, x', y, y'].
+    screen_edges_x, screen_edges_y : ndarray, shape (Ns,)
+        Coordinates of bin edges on the screen in [x, y].
+        
+    Returns
+    -------
+    Z : ndarray, shape (Nr**4)
+        Z[i, j, k, l] gives the phase space density at 
+        x = rec_grid_centers[0][i], 
+        x' = rec_grid_centers[1][j], 
+        y = rec_grid_centers[2][k], 
+        y' = rec_grid_centers[3][l].
+    """
+    
+    print('Forming arrays.')
+
+    # Treat each reconstruction bin center as a particle. 
+    rec_grid_coords = rec.get_grid_coords(*rec_grid_centers)
+    n_bins_rec = [len(c) for c in grid_centers]
+    rec_grid_size = np.prod(n_bins_rec)
+    col_indices = np.arange(rec_grid_size)
+
+
+    n_bins_x_screen = len(screen_edges_x) - 1
+    n_bins_y_screen = len(screen_edges_y) - 1
+    row_block_size = n_bins_x_screen * n_bins_y_screen
+    n_proj = len(projections)
+    rho = np.zeros(n_proj * row_block_size) # measured density on the screen.
+    rows, cols = [], [] # nonzero row and column indices of P
+
+    for proj_index in trange(n_proj):
+        # Transport the reconstruction grid to the screen.
+        M = tmats[proj_index]
+        screen_grid_coords = np.apply_along_axis(lambda row: np.matmul(M, row), 1, rec_grid_coords)
+
+        # For each particle, record the indices of the bin it landed in. So we want (k, l) such
+        # that the particle landed in the bin with x = x[k] and y = y[l] on the screen. One of 
+        # the indices will be -1 or n_bins if the particle landed outside the screen.
+        xidx = np.digitize(screen_grid_coords[:, 0], screen_edges_x) - 1
+        yidx = np.digitize(screen_grid_coords[:, 2], screen_edges_y) - 1
+        on_screen = np.logical_and(np.logical_and(xidx >= 0, xidx < n_bins_x_screen), 
+                                   np.logical_and(yidx >= 0, yidx < n_bins_y_screen))
+
+        # Get the indices for the flattened array.
+        projection = projections[proj_index]
+        screen_idx = np.ravel_multi_index((xidx, yidx), projection.shape, mode='clip')
+
+        # P[i, j] = 1 if particle j landed in bin i on the screen, 0 otherwise.
+        i_offset = proj_index * row_block_size
+        for j in tqdm(col_indices[on_screen]):
+            i = screen_idx[j] + i_offset
+            rows.append(i)
+            cols.append(j)
+        rho[i_offset: i_offset + row_block_size] = projection.flat
+
+
+    print('Creating sparse matrix P.')
+    t = time.time()
+    P = sparse.csr_matrix((np.ones(len(rows)), (rows, cols)), shape=(n_proj * row_block_size, rec_grid_size))
+    print('Done. t = {}'.format(time.time() - t))
+
+    print('Solving linear system.')
+    t = time.time()
+    psi, istop, itn, r1norm, r2norm, anorm, acond, arnorm, xnorm, var = sparse.linalg.lsqr(P, rho, show=True, iter_lim=1000)
+    print()
+    print('Done. t = {}'.format(time.time() - t))
+
+    print('Reshaping phase space density.')
+    Z = psi.reshape(tuple(n_bins_rec))
+    
+    return Z
