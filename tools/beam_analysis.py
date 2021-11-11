a1b2451beb89e01787229e1478c98c7af185befe
austin-hoover <ahoover7@tennessee.edu>
2021-11-11 11:53:14 -0500

    Add method to load PyORBIT bunch

--- tools/beam_analysis.py
+++ tools/beam_analysis.py
@@ -50,6 +50,15 @@ def get_ellipse_coords(env_params, npts=100):
     coords = np.vstack([x, xp, y, yp]).T
     return coords
 
+
+def _rms_ellipse_dims(sig_xx, sig_yy, sig_xy):
+    """Return semi-axes and tilt angle of the RMS ellipse in the x-y plane."""
+    angle = -0.5 * np.arctan2(2 * sig_xy, sig_xx - sig_yy)
+    sn, cs = np.sin(angle), np.cos(angle)
+    cx = np.sqrt(abs(sig_xx*cs**2 + sig_yy*sn**2 - 2*sig_xy*sn*cs))
+    cy = np.sqrt(abs(sig_xx*sn**2 + sig_yy*cs**2 + 2*sig_xy*sn*cs))
+    return angle, cx, cy
+
     
 def rms_ellipse_dims(Sigma, x1='x', x2='y'):
     """Return (angle, c1, c2) of rms ellipse in x1-x2 plane, where angle is the
@@ -269,3 +278,62 @@ class StatsReader:
     def dfs(self):
         return [self.twiss2D, self.twiss4D, self.moments, 
                 self.corr, self.realspace, self.env_params]
+
+    
+class TuneCalculator:
+    """This is a workaround while the PyORBIT method is broken.
+    
+    Note: we normalize by the lattice Twiss parameters, not the beam Twiss parameters. 
+    """
+    def __init__(self, mass, kin_energy, alpha_x, alpha_y, beta_x, beta_y, eta_x=0., eta_px=0.):
+        self.alpha_x = alpha_x
+        self.alpha_y = alpha_y
+        self.beta_x = beta_x
+        self.beta_y = beta_y
+        self.eta_x = eta_x
+        self.eta_px = eta_px
+        self.kin_energy = kin_energy
+        self.mass = mass
+        self.energy = self.kin_energy + self.mass
+        self.gamma = self.energy / self.mass
+        self.beta = 1 / np.sqrt(1 - (1 / self.gamma**2))
+        
+    def normalize(self, X):
+        Xn = np.copy(X)
+        for i, (x, xp, y, yp, z, dE) in enumerate(X):
+            dpp = (1 / self.beta**2) * dE / self.energy
+            xn = (x - self.eta_x * dpp) / np.sqrt(self.beta_x)
+            xpn = (xp - self.eta_px * dpp) * np.sqrt(self.beta_x) + xn * self.alpha_x
+            yn = y / np.sqrt(self.beta_y)
+            ypn = (yp + y * self.alpha_y / self.beta_y) * np.sqrt(self.beta_y)
+            Xn[i, :] = [xn, xpn, yn, ypn, z, dE]
+        return Xn
+
+    def get_phases(self, X): 
+        def _get_phases(u, up):
+            phases = np.arctan2(up, u)
+            phases[np.where(phases < 0.)] += (2 * np.pi)
+            return phases
+        X = self.normalize(X)
+        xphases = _get_phases(X[:, 0], X[:, 1])
+        yphases = _get_phases(X[:, 2], X[:, 3])
+        return np.vstack([xphases, yphases]).T
+    
+    def get_tunes(self, X0, X1):
+        phases0 = self.get_phases(X0)
+        phases1 = self.get_phases(X1)
+        # The bunches could be different sizes.
+        min_n_parts = min(len(phases0), len(phases1))
+        phases0 = phases0[:min_n_parts, :]
+        phases1 = phases1[:min_n_parts, :]
+        tunes = (phases0 - phases1) / (2 * np.pi)
+        tunes[np.where(tunes < 0.)] += 1.
+        return tunes
+    
+    
+def load_pybunch(filename):
+    X = pd.read_table(filename, sep=' ', skiprows=15, index_col=False, 
+                      names=['x','xp','y','yp','z','dE', 'mux', 'muy', 'nux', 'nuy', 'Jx', 'Jy'])
+    X.iloc[:, :4] *= 1000. # convert from m-rad to mm-mrad
+    X.iloc[:, 5] *= 1000. # convert energy spread from [GeV] to [MeV]
+    return X
