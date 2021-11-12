8d179595b3990d9b7ff1f0caf104fe4d582c4729
austin-hoover <ahoover7@tennessee.edu>
2021-11-12 09:41:56 -0500

    Make sure lsq_vs_replacebyavg is working

--- envelope/matching/convergence/lsq_vs_replacebyavg/sim.py
+++ envelope/matching/convergence/lsq_vs_replacebyavg/sim.py
@@ -8,6 +8,9 @@ lattice is a diagonal line in x-y space. If space charge is small but nonzero,
 the matched solution is approximately a diagonal line with an opposite tilt 
 angle. The packaged optimizer struggles with this case, but the 
 'replace by average' method is able to find the matched beam (not always).
+
+The 'replace by average' method is not guaranteed to converge. Use the
+least squares optimizer unless 
 """
 import sys
 import numpy as np
@@ -20,44 +23,46 @@ from orbit.analysis import add_analysis_nodes
 from orbit.analysis import get_analysis_nodes_data
 from orbit.envelope import DanilovEnvelope
 from orbit.lattice import AccLattice, AccNode, AccActionsContainer
-from orbit.space_charge.envelope import set_env_solver_nodes, set_perveance
+from orbit.space_charge.envelope import DanilovEnvSolverNode
+from orbit.space_charge.envelope import set_env_solver_nodes
+from orbit.space_charge.envelope import set_perveance
 from orbit.space_charge.sc2p5d.scLatticeModifications import setSC2p5DAccNodes
 from orbit.teapot import TEAPOT_Lattice
 from orbit.twiss import twiss
 from orbit.utils import helper_funcs as hf
+from orbit.utils.general import delete_files_not_folders
+from orbit.utils.consts import mass_proton
 
-sys.path.append('/Users/46h/Research/code/accphys') 
-from tools.utils import delete_files_not_folders
-    
-    
+
 # Settings
 #------------------------------------------------------------------------------
-# General
-mass = 0.93827231 # GeV/c^2
-kin_energy = 1.0 # GeV/c^2
-intensities = np.linspace(0, 1.5e14, 75)
-bunch_length = 5.0
-n_turns_track = 20
-
 # Lattice
-latfile = '_latfiles/fodo_skew_quadstart.lat'
-latseq = 'fodo'
-fringe = False
+madx_file = '_input/SNSring_nux6.18_nuy6.18.lat'
+madx_seq = 'rnginj'
+# madx_seq = 'fodo'
+print('madx_file = {}'.format(madx_file))
 
 # Initial beam
-mode = 1
-eps = 50e-6 # intrinsic emitance
-eps_x_frac = 0.5 # ex/eps
+mass = mass_proton # [GeV/c^2]
+kin_energy = 0.8 # [GeV]
+intensities = 1.5e14 * np.linspace(0., 0.8, 8)
+bunch_length = (45.0 / 64.0) * 248.0 # [m]
+mode = 2 # {1, 2} determines sign of angular momentum
+eps_l = 20e-6 # nonzero intrinsic emittance [m rad]
+eps_x_frac = 0.5 # eps_x / eps_l
 nu = np.radians(90) # x-y phase difference
 
 # Space charge solver
-max_solver_spacing = 0.05 # [m]
+max_solver_spacing = 0.1 # [m]
 min_solver_spacing = 1e-6
+gridpts = (128, 128, 1) # (x, y, z)
 
 # Matching
-match = True 
-tol = 1e-4
-verbose = 0
+tol = 1e-4 # absolute tolerance for cost function
+verbose = 2 # {0 (silent), 1 (report once at end), 2 (report at each step)}
+perturb_radius = 0. # between 0 (no effect) and 1.0
+method = 'replace_by_avg' 
+n_turns_track = 10
 
 
 delete_files_not_folders('_output/')
@@ -66,13 +71,15 @@ delete_files_not_folders('_output/')
 # Simulation
 #------------------------------------------------------------------------------
 cost_lists, matched_params_lists, tbt_params_lists = [], [], []
-env = DanilovEnvelope(eps, mode, eps_x_frac, mass, kin_energy, length=bunch_length)
+env = DanilovEnvelope(eps_l, mode, eps_x_frac, mass, kin_energy, length=bunch_length)
 for method in ('lsq', 'replace_avg'):
     print 'Method =', method
     cost_list = []
     tbt_params_list = [] 
     for intensity in tqdm(intensities):
-        lattice = hf.lattice_from_file(latfile, latseq, fringe)
+        lattice = TEAPOT_Lattice()
+        lattice.readMADX(madx_file, madx_seq)
+        lattice.set_fringe(False)
         env.match_bare(lattice)
         env.set_intensity(intensity)
         solver_nodes = set_env_solver_nodes(lattice, env.perveance, max_solver_spacing)
